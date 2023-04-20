d36f749c161418cc4cb92bddf4ee3c2a6a300064
Thomas Wanchai MENIER <thomas.menier@etu.u-bordeaux.fr>
2023-04-20 15:58:48 +0900

    style: better var names

--- pattern_tracking/utils.py
+++ pattern_tracking/utils.py
@@ -4,31 +4,34 @@ import cv2 as cv
 import numpy as np
 
 
-def alpha_blend(src1: cv.Mat, src2: cv.Mat) -> np.ndarray:
+def alpha_blend(bg: cv.Mat, fg: cv.Mat) -> np.ndarray:
     """
-    Merges two RGBA images together, in the OpenCV type
+    Merges two RGBA images together, by applying the image
+    'fg' on top of 'bg'
 
     See the following link for more details :
     https://stackoverflow.com/questions/60398939/how-to-do-alpha-compositing-with-a-list-of-rgba-data-in-numpy-arrays
-    :param src1: An RGBA image matrix
-    :param src2: The other RGBA image matrix to blend in
-    :return: An image with src1 applied on top of src2
+    :param bg: An RGBA image matrix
+    :param fg: The other RGBA image matrix to blend in
+    :return: An image with fg applied on top of bg
     """
 
     # alpha channels are normalized to a 0..1 range
+    # required because of the alpha blending formula
+    # see
     try:
-        src1_alpha = src1[..., 3] / 255.0
-        src2_alpha = src2[..., 3] / 255.0
+        bg_alpha = bg[..., 3] / 255.0
+        fg_alpha = fg[..., 3] / 255.0
     except IndexError:
         print("One of the images is not RGBA !", file=sys.stderr)
         exit(100)
 
     # extraction of RGB channels
-    src1_rgb = src1[..., :3]
-    src2_rgb = src2[..., :3]
+    bg_rgb = bg[..., :3]
+    fg_rgb = fg[..., :3]
 
     # computing resulting channels
-    result_alpha = src1_alpha + src2_alpha * (1 - src1_alpha)
+    result_alpha = bg_alpha + fg_alpha * (1 - bg_alpha)
     result_alpha[np.where(result_alpha == 0)] = 1  # avoid div by zero
 
     # note: broadcasting of the alpha channels is required here, so that numpy can
@@ -36,9 +39,9 @@ def alpha_blend(src1: cv.Mat, src2: cv.Mat) -> np.ndarray:
     # you can learn more about broadcasting numpy arrays on the official docs
     # https://numpy.org/doc/stable/user/basics.broadcasting.html
     result_rgb = 1.0 * (
-            src1_rgb * src1_alpha[:, :, np.newaxis]
-            + src2_rgb * src2_alpha[:, :, np.newaxis]
-            * (1 - src1_alpha[:, :, np.newaxis])
+            bg_rgb * bg_alpha[:, :, np.newaxis]
+            + fg_rgb * fg_alpha[:, :, np.newaxis]
+            * (1 - bg_alpha[:, :, np.newaxis])
         ) / result_alpha[:, :, np.newaxis]
 
     # merging into result image
@@ -49,10 +52,10 @@ def alpha_blend(src1: cv.Mat, src2: cv.Mat) -> np.ndarray:
 
 
 if __name__ == '__main__':
-    fg = cv.imread('assets/KTIfd.png', cv.IMREAD_UNCHANGED)
-    bg = cv.imread('assets/Tyxgv.png', cv.IMREAD_UNCHANGED)
+    fore = cv.imread('assets/KTIfd.png', cv.IMREAD_UNCHANGED)
+    back = cv.imread('assets/Tyxgv.png', cv.IMREAD_UNCHANGED)
 
-    res = alpha_blend(bg, fg)
+    res = alpha_blend(back, fore)
     cv.imshow('', res)
     cv.waitKey(-1)
     cv.destroyAllWindows()
