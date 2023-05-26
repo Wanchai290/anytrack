ea9a2a37546c67386f2556e65e884633158cc15a
Thomas Wanchai MENIER <thomas.menier@etu.u-bordeaux.fr>
2023-05-26 16:09:13 +0900

    wip: example of live-modified plot

--- qt5_gui/main.py
+++ qt5_gui/main.py
@@ -1,18 +1,31 @@
 import sys
+import time
+from threading import Thread
 
+import numpy as np
 from PySide6.QtGui import QWindow
 from PySide6.QtWidgets import QApplication, QVBoxLayout, QMainWindow
 from pyqtgraph import PlotDataItem, PlotItem, PlotWidget
+
+
+def update_plot(plot_data_item: PlotDataItem):
+    time.sleep(2)
+    plot_data_item.setData([(i + int(i / 4)) * i for i in range(10, 0, -1)])
+    return
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = QMainWindow()
 
     somePlotItem = PlotItem()
-    somePlotItem.plot([(i + int(i / 4)) * i for i in range(10)])
+    plotDataItem = somePlotItem.plot([(i + int(i / 4)) * i for i in range(10)])
     plotWidget = PlotWidget(plotItem=somePlotItem)
 
     window.setCentralWidget(plotWidget)
     window.show()
+
+    th = Thread(target=update_plot, args=(plotDataItem, ))
+    th.start()
 
     app.exec()
