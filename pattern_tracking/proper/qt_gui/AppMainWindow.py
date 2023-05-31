76ebab2692da09e44f6ad3856061d1e38b937c01
Thomas Wanchai MENIER <thomas.menier@etu.u-bordeaux.fr>
2023-05-31 18:20:12 +0900

    feat: plotter widget on the side

--- pattern_tracking/proper/qt_gui/AppMainWindow.py
+++ pattern_tracking/proper/qt_gui/AppMainWindow.py
@@ -1,6 +1,8 @@
+from PySide6.QtCore import Qt
 from PySide6.QtGui import QPixmap, QAction
-from PySide6.QtWidgets import QMainWindow, QMenu
+from PySide6.QtWidgets import QMainWindow, QMenu, QWidget
 
+from pattern_tracking.proper.qt_gui.dock_widgets.LivePlotterDockWidget import LivePlotterDockWidget
 from pattern_tracking.proper.qt_gui.top_menu_bar.trackers.CreateTrackerAction import CreateTrackerAction
 from pattern_tracking.proper.qt_gui.top_menu_bar.trackers.TrackersMenu import TrackersMenu
 from pattern_tracking.proper.tracker.TrackerManager import TrackerManager
@@ -15,14 +17,13 @@ class AppMainWindow(QMainWindow):
 
     def __init__(self, tracker_manager: TrackerManager, frame_shape: tuple[int, int, int]):
         super().__init__()
-
         self._TRACKER_MANAGER = tracker_manager
         self._FRAME_DISPLAY = FrameDisplayWidget(tracker_manager, frame_shape)
-        # self._live_dist_plot: QWidget =
+        self._LIVE_DISTANCE_PLOT = LivePlotterDockWidget(self)
         self._TRACKERS_MENU = TrackersMenu(tracker_manager, parent=self)
 
         self.menuBar().addMenu(self._TRACKERS_MENU)
-
+        self.addDockWidget(Qt.RightDockWidgetArea, self._LIVE_DISTANCE_PLOT)
         self.setCentralWidget(self._FRAME_DISPLAY)
 
     def get_frame_display_widget(self) -> FrameDisplayWidget:
---
+++ pattern_tracking/proper/qt_gui/dock_widgets/LivePlotterDockWidget.py
@@ -0,0 +1,22 @@
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QDockWidget, QWidget, QLabel, QGraphicsView, QHBoxLayout
+from pyqtgraph import PlotWidget, PlotItem
+
+
+class LivePlotterDockWidget(QDockWidget):
+
+    def __init__(self, parent: QWidget | None = None):
+        super().__init__(parent)
+        self.setFeatures(QDockWidget.DockWidgetFeature.DockWidgetClosable)
+
+        self._plot_widgets: list[PlotWidget] = []
+
+        tmp_widget = QWidget()
+        tmp_layout = QHBoxLayout()
+        tmp_layout.addWidget(QLabel("No plot defined !"))
+        tmp_layout.setAlignment(Qt.AlignmentFlag.AlignVCenter | Qt.AlignmentFlag.AlignHCenter)
+        tmp_widget.setLayout(tmp_layout)
+        self.setWidget(tmp_widget)
+        self.setMinimumSize(800, 240)
+        self.show()
