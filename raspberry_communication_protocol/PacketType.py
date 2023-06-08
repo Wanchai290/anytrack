e08e3d8ba8400c639bb3490a1f211b85568a6e4b
Thomas Wanchai MENIER <thomas.menier@etu.u-bordeaux.fr>
2023-06-08 13:45:48 +0900

    feat: changed semantics of packet types

--- raspberry_communication_protocol/PacketType.py
+++ raspberry_communication_protocol/PacketType.py
@@ -2,7 +2,15 @@ from enum import Enum
 
 
 class PacketType(Enum):
-    ACCEPT = 0b00
-    BUFFER_START = 0b01
-    BUFFER_END = 0b10
+    """
+    Represents which type of packet is being sent.
+
+    OK: The receiver has correctly received the payload \n
+    FRAME: The payloads contains an image frame \n
+    HALT: One of the clients requests to end the connection \n
+    REQUEST_AGAIN: Requests to send the last packet one more time, because it was damaged during transmission
+    """
+    OK = 0b00
+    FRAME = 0b01
+    HALT = 0b10
     REQUEST_AGAIN = 0b11
