a94b8bddce04f8ab8c916f3f575050d60b85ddb9
Thomas Wanchai MENIER <thomas.menier@etu.u-bordeaux.fr>
2023-06-11 12:23:02 +0900

    fix: clearing plot does not plot a new point at (0, 0)
    i've also moved most data functions into DistancePlotWidget. Not ideal, but better anyway

--- pattern_tracking/proper/qt_gui/dock_widgets/LivePlotterDockWidget.py
+++ pattern_tracking/proper/qt_gui/dock_widgets/LivePlotterDockWidget.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QDockWidget, QWidget, QLabel, QHBoxLayout
-from pyqtgraph import PlotWidget
+from pyqtgraph import PlotWidget, PlotItem
 
 from pattern_tracking.proper.logic.DistanceComputer import DistanceComputer
 from pattern_tracking.proper.qt_gui.top_menu_bar.plot.DistancePlotWidget import DistancePlotWidget
@@ -15,6 +15,8 @@ class LivePlotterDockWidget(QDockWidget):
     Dock widget that contains a collection of PlotWidget objects, alongside their
     affected DistanceComputer object that give them the additional data to plot to the user
     """
+
+    WIDGET_SIZE = 800, 240
 
     def __init__(self, parent: QWidget | None = None):
         super().__init__(parent)
@@ -30,15 +32,19 @@ class LivePlotterDockWidget(QDockWidget):
         tmp_widget.setLayout(tmp_layout)
 
         self.setWidget(tmp_widget)
-        self.setMinimumSize(800, 240)
+        self.setMinimumSize(*LivePlotterDockWidget.WIDGET_SIZE)
         self.show()
 
     def new_plot(self, dist_computer: DistanceComputer, feed_fps: int):
         """Create a new PlotWidget to be displayed, and updated in real-time"""
         set_new_as_active = len(self._plot_widgets) == 0
         plot_widget = DistancePlotWidget(feed_fps)
-        self._mutex.acquire()  # because of frame number attribute
-        LivePlotterDockWidget.plot_new_point(plot_widget, dist_computer.distance(), self._current_frame_number, update=False)
+
+        # Here, this mutex was required because of the self._current_frame_number attribute
+        # it only changes when called by the update_plots() method.
+        self._mutex.acquire()
+        LivePlotterDockWidget.plot_new_point(plot_widget, dist_computer.distance(), self._current_frame_number,
+                                             update=False)
         self._mutex.release()
         self._plot_widgets[dist_computer] = plot_widget
 
@@ -53,36 +59,30 @@ class LivePlotterDockWidget(QDockWidget):
         for (dist_computer, plot_widget) in self._plot_widgets.items():
             distance = dist_computer.distance()
             if distance != DistanceComputer.ERR_DIST:
-                LivePlotterDockWidget.plot_new_point(plot_widget, distance, self._current_frame_number, update=True)
+                plot_widget.plot_new_point(plot_widget.get_feed_fps(), distance, frame_number)
         self._mutex.release()
 
     def clear_all(self):
         self._mutex.acquire()
         for (dist_computer, plot_widget) in self._plot_widgets.items():
-            plot_data_item = plot_widget.plotItem.listDataItems()[0]
-            plot_data_item.setData([dist_computer.distance()])
+            plot_widget.clear()
         self._mutex.release()
-
-    @staticmethod
-    def _x_time_computation(fps: int, frame_number: int) -> float:
-        return frame_number / fps
 
     @classmethod
     def plot_new_point(cls, plot_widget: DistancePlotWidget, distance: float, current_frame_number: int,
                        update: bool = False):
-        feed_fps = plot_widget.get_fps()
-        new_x = LivePlotterDockWidget._x_time_computation(feed_fps, current_frame_number)
-        new_y = distance
-
-        if update:
-            # a PlotItem can contain multiple PlotDataItem objects
-            # we only put 1 unique PlotDataItem in each plot
-            plot_data_item = plot_widget.plotItem.listDataItems()[0]
-            data_x, data_y = plot_data_item.getData()
-            data_x = np.append(data_x, new_x)
-            data_y = np.append(data_y, int(new_y))
-            plot_data_item.setData(data_x, data_y)
-
-        else:
-            plot_widget.plotItem.plot([new_x], [new_y])
+        """Plots a new point for a PlotWidget object"""
+        # plot_widget.plot_new_point(plot_widget.get_fps(), distance, current_frame_number)
+        # new_x, new_y = LivePlotterDockWidget.new_point_data(feed_fps, distance, current_frame_number)
+        # if update:
+        #     # a PlotItem can contain multiple PlotDataItem objects
+        #     # we only put 1 unique PlotDataItem in each plot
+        #     plot_data_item = plot_widget.plotItem.listDataItems()[0]
+        #     data_x, data_y = plot_data_item.getData()
+        #     data_x = np.append(data_x, new_x)
+        #     data_y = np.append(data_y, int(new_y))
+        #     plot_data_item.setData(data_x, data_y)
+        #
+        # else:
+        #     plot_widget.plotItem.plot([new_x], [new_y])
 
--- pattern_tracking/proper/qt_gui/top_menu_bar/plot/DistancePlotWidget.py
+++ pattern_tracking/proper/qt_gui/top_menu_bar/plot/DistancePlotWidget.py
@@ -1,9 +1,15 @@
 from typing import Any
 
 from pyqtgraph import PlotWidget
+import numpy as np
 
 
 class DistancePlotWidget(PlotWidget):
+    """
+    This custom widget displays a live distance graph.
+    Note that we assume that we only use ONE PlotDataItem
+    in this widget's PlotData.
+    """
 
     def __init__(self,
                  feed_fps: int,
@@ -12,7 +18,44 @@ class DistancePlotWidget(PlotWidget):
                  plotItem: Any = None,
                  **kargs: Any) -> None :
         super().__init__(parent, background, plotItem, **kargs)
-        self._fps = feed_fps
+        self._feed_fps = feed_fps
+        self._initialized = False
 
-    def get_fps(self):
-        return self._fps
+    def get_feed_fps(self):
+        return self._feed_fps
+
+    def plot_new_point(self, feed_fps: int, distance: float,
+                       current_frame_number: int):
+        """Plots a new point with the given data to this plot"""
+        new_x, new_y = DistancePlotWidget.new_point_data(feed_fps, distance, current_frame_number)
+
+        # the only way to create a PlotDataItem for a PlotItem
+        # is calling PlotItem.plot()
+        # we need to know whether we have to update the plot
+        # or to initialize the PlotDataItem behind
+        # we check whether there is one PlotDataItem in this PlotItem
+        # below line is similar to `if self.plotItem.listDataItems() != []`
+        if self.plotItem.listDataItems():
+            # a PlotItem can contain multiple PlotDataItem objects
+            # we only put 1 unique PlotDataItem in each plot
+            plot_data_item = self.plotItem.listDataItems()[0]
+            data_x, data_y = plot_data_item.getData()
+            data_x = np.append(data_x, new_x)
+            data_y = np.append(data_y, int(new_y))
+            plot_data_item.setData(data_x, data_y)
+
+        else:
+            self.plotItem.plot([new_x], [new_y])
+            self._initialized = True
+
+    def clear(self):
+        """Removes the only PlotDataItem used in this PlotWidget"""
+        self.plotItem.clear()
+        self._initialized = False
+
+    @classmethod
+    def new_point_data(cls, feed_fps: int, distance: float, current_frame_number: int):
+        """Contains the current formulas used to compute new values on the x and y-axis"""
+        new_x = current_frame_number / feed_fps
+        new_y = distance
+        return new_x, new_y
