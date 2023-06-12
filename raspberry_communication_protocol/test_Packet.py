b9c2dc94d78d8c5d9a208cb58a3a8b624e8de4e1
Thomas Wanchai MENIER <thomas.menier@etu.u-bordeaux.fr>
2023-06-12 15:06:09 +0900

    test: fixed tests for Packet class

--- raspberry_communication_protocol/test_Packet.py
+++ raspberry_communication_protocol/test_Packet.py
@@ -10,7 +10,7 @@ class Test(TestCase):
 
     def setUp(self) -> None:
         super().setUp()
-        self._p_data = (1, PacketType.OK, np.array(((5, 5), (4, 4))))
+        self._p_data = (1, PacketType.OK, np.zeros((2, 2, 3), dtype=int))
         self._p = Packet(*self._p_data)
 
     def test_no_change_after_transfer(self):
@@ -18,17 +18,27 @@ class Test(TestCase):
         p_deser = Packet.deserialize(p_ser)
         self.assertEqual(self._p, p_deser)
 
-    def test_valid_after_serialize_deserialize(self):
-        p_ser = self._p.serialize()
-        p_deser = Packet.deserialize(p_ser)
-        self.assertTrue(p_deser)
-
     def test_eq_override(self):
         p_copy = Packet(*self._p_data)
         self.assertEquals(self._p, p_copy)
 
-    def test_invalid_if_crc_changed(self):
-        p_ser = self._p.serialize()
-        p_deser = Packet.deserialize(p_ser)
-        p_deser.payload_crc = 0
-        self.assertFalse()
+    def test_types_valid_after_deser(self):
+        p_deser = Packet.deserialize(self._p.serialize())
+        self.assertEqual(type(p_deser.frame_number), type(self._p.frame_number))
+        self.assertEqual(type(p_deser.packet_type), type(self._p.packet_type))
+        self.assertEqual(type(p_deser.frame_shape), type(self._p.frame_shape))
+        self.assertEqual(type(p_deser.frame_channel_count), type(self._p.frame_channel_count))
+        self.assertEqual(type(p_deser.payload), type(self._p.payload))
+        self.assertEqual(type(p_deser.payload_crc), type(self._p.payload_crc))
+
+    def test_serialize(self):
+        p = Packet(0xFA, PacketType.FRAME, np.zeros((2, 2, 3), dtype=int))
+        p_ser = p.serialize()
+        self.assertEqual(p_ser, b'inu\x9c\x00\x00\x00\xfa\x00\x02\x00\x02\x00\x00\x00`\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00neko')
+        self.assertEqual(Packet.deserialize(p_ser), p)
+
+        p = Packet(0xFA, PacketType.FRAME, np.full((2, 2, 3), 4, dtype=int))
+        p_ser = p.serialize()
+        self.assertEqual(p_ser, b'inu\x9c\x00\x00\x00\xfa\x00\x02\x00\x02\x00\x00\x00`\x04\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x002\rneko')
+        print(Packet.deserialize(p_ser) == p)
+        self.assertEqual(Packet.deserialize(p_ser), p)
