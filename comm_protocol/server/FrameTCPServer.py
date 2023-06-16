7d729b1d981ec62b84579b4042a4b700dd2f2243
Thomas Wanchai MENIER <thomas.menier@etu.u-bordeaux.fr>
2023-06-16 09:12:22 +0900

    fix: forgot logger name when initializing logger

--- comm_protocol/server/FrameTCPServer.py
+++ comm_protocol/server/FrameTCPServer.py
@@ -13,7 +13,7 @@ class FrameTCPServer(TCPServer):
     def __init__(self, frames_queue: Queue[int, ndarray], *args):
         super().__init__(*args)
         logging.basicConfig(level=logging.NOTSET)
-        self._logger = logging.getLogger()
+        self._logger = logging.getLogger(FrameTCPServer.LOGGER_NAME)
         self._logger.info(f"Server initialized and bound to {args[0]}")
         self.frames_queue = frames_queue
         self.current_data_to_send = None
